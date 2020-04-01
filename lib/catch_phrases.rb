04387d31616a421e552d39361190111448584bd4
Ian Rosen <irosen419@gmail.com>
2020-04-01 15:28:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,15 @@
+$phrase = "It's-a me, Mario!"
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$says = "It's Dangerous To Go Alone! Take This."
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It's-a me, Mario!"
-  puts phrase
+  puts $phrase
 end
 
 def toadstool
-  puts status
+  puts $status
+end
+
+def link
+  puts says
 end
